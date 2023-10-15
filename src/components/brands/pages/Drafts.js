0ed7c4d1cdb6ad9cbd17d20b62d5ae81ee22c8f4
Benson Ndiwa <bnkimtai@gmail.com>
2023-10-16 00:39:36 +0300

    Drafts Table Added

--- src/components/brands/pages/Drafts.js
+++ src/components/brands/pages/Drafts.js
@@ -27,45 +27,59 @@ function Drafts() {
     <div className='w-full min-h-screen bg-neutral-300'>
         <Navbar />
         <div className='p-4 ml-16'>
-            <h1 className='text-sm mb-3 p-3 uppercase font-bold text-gray-700'>Drafts</h1>
+            <h1 className='text-sm mb-3 p-3 uppercase font-bold text-gray-700 text-center'>Drafts</h1>
 
-            <div className='flex flex-wrap gap-4'>
-            { 
+              <div class="relative overflow-x-auto shadow-md sm:rounded-lg lg:w-3/4 flex lg:mx-auto">
+                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                  <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">                      
+                        <tr>
+                          <th scope="col" class="px-3 py-1 lg:px-6 lg:py-3">
+                            Image
+                          </th>
+                          <th scope="col" class="px-3 py-1 lg:px-6 lg:py-3">
+                              Campaign Title
+                          </th>
+                          <th scope="col" class="px-3 py-1 lg:px-6 lg:py-3">
+                              Date Created
+                          </th>
+                          <th scope="col" class="px-3 py-1 lg:px-6 lg:py-3">
+                              Edit
+                          </th>
+                          <th scope="col" class="px-3 py-1 lg:px-6 lg:py-3">
+                              Delete
+                          </th>
+                      </tr>
+                  </thead>
+                  <tbody>
+
+                  { 
                 data.length > 0 && data.map( item => (
-                            <Link to={`/view_campaign/${item._id}`}>
-                            <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                                <img src={`${process.env.REACT_APP_API_URL}/uploads/${item.cover}`} class="p-0 rounded-t-lg  h-56"  alt="No image Uploaded"  />
-                                
+                    <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                        <td class="px-3 py-1 lg:px-6 lg:py-3">
+                        <img src={`${process.env.REACT_APP_API_URL}/uploads/${item.cover}`} class="p-0 rounded-t-lg h-20 m-2"  alt="No image Uploaded"  />
+                        </td>
+                        <th scope="row" class="px-3 py-1 lg:px-6 lg:py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">                              
+                          {item.title}
+                          </th>
+                          <td scope="col" class="px-3 py-1 lg:px-6 lg:py-3">
+                              {item.startDate}
+                          </td>
+                          <td scope="col" class="px-3 py-1 lg:px-6 lg:py-3">
+                              <Link to={`/edit_influencer_campaign/${id}/${item._id}`} className='bg-blue-600 hover:bg-blue-500 text-white rounded-lg shadow-md p-4'>
+                                Edit
+                              </Link>
+                          </td>
+                          <td scope="col" class="px-3 py-1 lg:px-6 lg:py-3">
+                          <button className='bg-red-600 hover:bg-red-500 text-white rounded-lg shadow-md p-4'>Delete</button>
+                          </td>
+                      </tr>
+                ))
+                }
+                     
+                  </tbody>
+              </table>
+          </div>
         
-                                <div class="px-5 pb-5">
-                                    <div className='flex justify-between items-center '>
-                                        <h5 class="text-2xl font-semibold tracking-tight text-gray-900 dark:text-white mt-2 overflow-ellipsis">{item.title}</h5>
-                                        <Link to={`/view_campaign/${item._id}`} className='mt-2 w-1/4'><VisibilityIcon /></Link>
-                                    </div>
-                                    
-                                    <div className=" text-sm font-semibold mr-2 py-0.5 rounded text-sky-900 mb-1 mt-1 capitalize">
-                                        {item.industry}
-                                    </div>
-                                    <i className=" text-sm font-semibold mr-2 py-0.5 rounded text-red-800 mb-1 mt-1 capitalize">
-                                        {item.type} Campaign
-                                    </i>
-                                    <br />
-                                    <div className="bg-sky-200 text-md font-semibold mr-2 px-1 py-0.5 rounded text-slate-900 mb-3 mt-2 inline-block capitalize font-mono">
-                                        {item.status}
-                                    </div>
-                                    <div class="flex items-center justify-between align-middle">
-                                        <span class="text-2xl font-bold text-sky-900 dark:text-white">$ {item.budget}</span>
-                                        <Link to={`/edit_influencer_campaign/${id}/${item._id}`} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Edit Campaign</Link>
-                                    </div>
-                                </div>
-                            </div>
-                        </Link>
-                      )
-                    
-                )
-            }
-            
-            </div>           
             
         </div>
     </div>
